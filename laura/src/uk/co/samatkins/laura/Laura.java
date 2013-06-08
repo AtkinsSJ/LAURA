2223b34a179e436c44c8125675222fd2a86a99c4
Sam Atkins <github@samatkins.co.uk>
2013-06-08 14:38:43 +0100

    LAURA loads modules from the ~/.laura/plugins directory! :D

--- laura/src/uk/co/samatkins/laura/Laura.java
+++ laura/src/uk/co/samatkins/laura/Laura.java
@@ -2,7 +2,13 @@ package uk.co.samatkins.laura;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class Laura {
 	
@@ -14,11 +20,20 @@ public class Laura {
 		modules = new ArrayList<Module>();
 	}
 	
+	public void print(String output) {
+		System.out.println(output);
+	}
+	
 	public void start() {
-		System.out.println("Waking LAURA...");
+		print("Waking LAURA...");
 		createUserDir();
-		loadPlugins();
-		System.out.println("LAURA is awake!");
+		try {
+			loadPlugins();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		print("LAURA is awake!");
 	}
 	
 	/**
@@ -28,14 +43,16 @@ public class Laura {
 		homeDir = new File(System.getProperty("user.home") + File.separator + ".laura");
 		if (!homeDir.isDirectory()) {
 			if (homeDir.mkdir()) {
-				System.out.println("Created .laura directory");
+				print("Created .laura directory");
 			} else {
-				System.out.println("COULD NOT CREATE .laura DIRECTORY!");
+				print("COULD NOT CREATE .laura DIRECTORY!");
 			}
 		}
 	}
 	
-	private void loadPlugins() {
+	private void loadPlugins() throws IOException {
+		print("Loading modules");
+		
 		File pluginsDir = new File(homeDir, "plugins");
 		// Create plugins folder if it doesn't exist
 		if (!pluginsDir.isDirectory()) {
@@ -50,7 +67,50 @@ public class Laura {
 			}
 		});
 		
+		if (pluginFiles.length == 0) {
+			print("ERROR: No modules found!");
+			return;
+		}
 		
+		URLClassLoader classLoader;// = new URLClassLoader(new URL[]{pluginsDir.toURI().toURL()});
+
+		// Iterate through .jar files
+		JarFile jarFile;
+		for (File plugin: pluginFiles) {
+			classLoader = new URLClassLoader(new URL[]{plugin.toURI().toURL()});
+			jarFile = new JarFile(plugin);
+			
+			// Search through the JAR for class files
+			Enumeration<JarEntry> entries = jarFile.entries();
+			while (entries.hasMoreElements()) {
+				JarEntry element = entries.nextElement();
+				if (element.getName().endsWith(".class")) {
+					try {
+						// Instantiate a module and add it to Laura.modules
+						Class<?> c = Class.forName(
+							element.getName().replaceAll(".class", "").replaceAll("/", "."),
+							true, classLoader
+						);
+						Module m = (Module)c.newInstance();
+						m.init();
+						modules.add(m);
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InstantiationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ClassCastException e) {
+						
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		print("Successfully loaded " + modules.size() + " modules");
 	}
 
 }
